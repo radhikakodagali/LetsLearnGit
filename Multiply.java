ea59fd1757a4b39dc48190cf0b766bc137b0921d
Radhika <radhikakodagali@gmail.com>
2022-07-30 19:25:44 +0530

    multiply

--- Multiply.java
+++ Multiply.java
@@ -2,7 +2,7 @@ public class Multiply{
     public static void main(String[] args){
         int a=10;
         int b=20;
-        System.out.println("sum");
+        System.out.println("sum1");
 
     }
 }
